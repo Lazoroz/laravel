ad279366d7c5eec355fed44140fb8c59095fcb7a
Lazoroz <ramlal524@gmail.com>
2023-05-17 18:40:17 +0200

    added mentor

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -13,7 +13,7 @@ class MentorController extends Controller
      */
     public function index()
     {
-        return view();
+        return view('mentor.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +21,14 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth:web', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:web')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('mentor', MentorController::class);//->middleware('auth:admin');
 
 require __DIR__.'/auth.php';
